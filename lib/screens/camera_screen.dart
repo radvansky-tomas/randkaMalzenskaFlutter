ab34884003f6b1318345c23ed7a29b2f4f911d57
Maciej Zboina <maciej.zboina@prime-force.com>
2021-07-14 22:48:38 +0200

    test json gmail

---
+++ lib/screens/camera_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  //final CameraDescription camera;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
