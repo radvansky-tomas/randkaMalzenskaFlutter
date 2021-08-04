d80c5732567cb5f855bc625175772de586ba2931
Maciej Zboina <maciej.zboina@prime-force.com>
2021-08-05 00:46:54 +0200

    expandable view on app bar

--- lib/screens/step/step_screen.dart
+++ lib/screens/step/step_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:randka_malzenska/models/sub_step.dart';
 import 'package:randka_malzenska/services/rest/connection_service.dart';
 import 'package:randka_malzenska/shared/button/custom_image_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String _prefKey = 'selectedStep';
 
 class StepScreen extends StatefulWidget {
   @override
@@ -10,44 +13,102 @@ class StepScreen extends StatefulWidget {
 
 class _StepScreenState extends State<StepScreen> {
   ConnectionService connectionService = new ConnectionService();
-  late List<SubStep>? subSteps = [];
+  Future<List<SubStep>?>? subSteps;
+  late SharedPreferences prefs;
+  int? _index;
+  bool _visible = false;
 
   @override
   void initState() {
     super.initState();
-    connectionService
-        .getSubstepsFromStep(0)
-        .then((value) => subSteps = value)
-        .whenComplete(() {
+    subSteps = connectionService.getSubstepsFromStep(0);
+    _initializePreferences().whenComplete(() {
       setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      body: subSteps != null && subSteps!.length == 3
-          ? Center(child: sampleBody())
-          : Center(
-              child: CircularProgressIndicator(),
+    return FutureBuilder<List<SubStep>?>(
+      future: connectionService.getSubstepsFromStep(0),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Stack(
+            children: [
+              Scaffold(
+                appBar: AppBar(
+                  leadingWidth: 150,
+                  leading: PopupMenuButton(
+                      icon: Text('Dzień 1'),
+                      itemBuilder: (context) => [
+                            ...snapshot.data!.map((e) {
+                              return PopupMenuItem(
+                                  child: Text(
+                                e.label,
+                                style: TextStyle(color: Colors.black),
+                              ));
+                            }).toList()
+                          ]),
+                  actions: <Widget>[],
+                ),
+                backgroundColor: Colors.blue,
+                body: Center(
+                  child: sampleBody(snapshot.data),
+                ),
+              ),
+              Visibility(
+                visible: _visible,
+                child: new Container(
+                  height: 100.0,
+                  width: 100.0,
+                  alignment: Alignment.center,
+                  color: Colors.redAccent,
+                  child: Text('Hello'),
+                ),
+              ),
+            ],
+          );
+        } else
+          return Scaffold(
+            backgroundColor: Colors.blue,
+            body: Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
             ),
+          );
+      },
     );
   }
 
-  Widget sampleBody() {
+  Widget sampleBody(List<SubStep>? awaitedSubSteps) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        ...(subSteps!).map((subStep) {
+        ...(awaitedSubSteps!).map((subStep) {
           return CustomImageButton(subStep, () => test(subStep));
         }).toList()
       ],
     );
   }
 
+  void showContainer() {
+    setState(() {
+      if (_visible) {
+        _visible = false;
+      } else {
+        _visible = true;
+      }
+    });
+  }
+
   void test(SubStep subStep) {
     print(subStep.label);
+  }
+
+  _initializePreferences() async {
+    prefs = await SharedPreferences.getInstance();
+    _index = prefs.getInt(_prefKey);
   }
 }
