346d124d8af1e6ce7fa7d44a55cae2d65be82c49
Maciej Zboina <maciej.zboina@prime-force.com>
2021-09-18 20:22:51 +0200

    splash screen 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,45 +2,22 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:randka_malzenska/models/preferences_key.dart';
 import 'package:randka_malzenska/providers/auth.dart';
-import 'package:randka_malzenska/screens/auth_screen.dart';
-import 'package:randka_malzenska/screens/registration/registry_gender_screen.dart';
-import 'package:randka_malzenska/screens/video/video_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:randka_malzenska/screens/splash_screen/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  runApp(MyApp(prefs));
-}
-
-Future selectNotification(String? payload) async {
-  if (payload != null) {
-    debugPrint('notification payload: $payload');
-  }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final SharedPreferences prefs;
-
-  MyApp(this.prefs);
   @override
   Widget build(BuildContext context) {
-    bool introWatched = prefs.getBool(PreferencesKey.introWatched) ?? false;
     return MultiProvider(
       providers: [ChangeNotifierProvider.value(value: Auth())],
       child: Consumer<Auth>(
-        builder: (context, auth, _) => MaterialApp(
-            home: introWatched
-                ? AuthScreen()
-                : VideoScreen(
-                    'https://player.vimeo.com/external/490901113.hd.mp4?s=eb884fbcbeb5f5f751a2f1754d649a6b0b2f7628&profile_id=175',
-                    RegistryUserDataScreen(prefs),
-                    false,
-                  )),
-      ),
+          builder: (context, auth, _) => MaterialApp(home: SplashScreen())),
     );
   }
 }
---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,120 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:randka_malzenska/models/preferences_key.dart';
+import 'package:randka_malzenska/providers/auth.dart';
+import 'package:randka_malzenska/screens/auth_screen.dart';
+import 'package:randka_malzenska/screens/registration/registry_gender_screen.dart';
+import 'package:randka_malzenska/screens/step/step_screen.dart';
+import 'package:randka_malzenska/screens/video/video_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late SharedPreferences prefs;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<User?> getUser() async {
+    var authBloc = Provider.of<Auth>(context, listen: false);
+    prefs = await SharedPreferences.getInstance();
+    await Future.delayed(const Duration(seconds: 5));
+    return authBloc.currentUser.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getUser(),
+      builder: (context, AsyncSnapshot snapshot) {
+        // Show splash screen while waiting for app resources to load:
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(home: Splash());
+        } else {
+          // Loading is done, return the app:
+          bool introWatched =
+              prefs.getBool(PreferencesKey.introWatched) ?? false;
+          User? user = snapshot.data;
+          return MaterialApp(
+            theme: ThemeData(
+              textTheme: GoogleFonts.montserratTextTheme(
+                Theme.of(context).textTheme,
+              ),
+            ),
+            home: user != null
+                ? StepScreen(user)
+                : introWatched
+                    ? AuthScreen()
+                    : VideoScreen(
+                        'https://player.vimeo.com/external/490901113.hd.mp4?s=eb884fbcbeb5f5f751a2f1754d649a6b0b2f7628&profile_id=175',
+                        RegistryUserDataScreen(prefs),
+                        false,
+                      ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  late Animation<Offset> animation;
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    animation = Tween<Offset>(
+      begin: Offset(-1.5, 0.0),
+      end: Offset(0.0, 0.0),
+    ).animate(CurvedAnimation(
+      parent: animationController,
+      curve: Curves.fastLinearToSlowEaseIn,
+    ));
+
+    Future<void>.delayed(Duration(milliseconds: 500), () {
+      animationController.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Stack(
+          children: [
+            Image.asset('assets/images/splashscreen2_bg.jpg'),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SlideTransition(
+                position: animation,
+                child: Image.asset('assets/images/splashscreen2.png'),
+              ),
+            )
+          ],
+        ));
+  }
+}
